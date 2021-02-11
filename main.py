b4c8c9029b396db93231c26b07625744e23f4168
gustavs-vagalis <gustavs.vagalis@gmail.com>
2021-02-11 11:42:26 +0200

    No longer links to repo

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ client_id = str(sys.argv[1])  # Reads client ID from argument #1
 print(sys.argv)
 RPC = Presence(client_id)  # Initialize the Presence client
 RPC.connect()
-buttons = [{"label": "Join the House of Bingus", "url": "https://discord.gg/bingus"},  # Clickable RP buttons
-           {"label": "Get this status for yourself", "url": "https://github.com/gustavs-vagalis/bingus-richpresence"}]
+buttons = [{"label": "Join the House of Bingus", "url": "https://discord.gg/bingus"}]
 
 while True:
     RPC.update(
-        details="My beloved",
-        large_image=str(sys.argv[2]),  # Reads image key from argument #2
+        large_image=str(sys.argv[2]),
+        details="My beloved",  # Reads image key from argument #2
         large_text="Bingus",
         buttons=buttons
     )
